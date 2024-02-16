6e186eb59f84963a9d26f4c6652fdba0e6322e00
tbog1997kom@gmail.com <tbog1997kom@gmail.com>
2024-02-15 19:39:47 -0500

    git

--- oddsscraper.py
+++ oddsscraper.py
@@ -1,11 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 
-headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
-url = "https://en.stoiximan.gr/sport/soccer/next-3-hours/"
+HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
+URL = "https://en.stoiximan.gr/sport/soccer/next-3-hours/"
 
-page = requests.get(url, headers=headers)
 
-soup = BeautifulSoup(page.text, 'html.parser')
-print(soup)
+#page = requests.get(URL, headers=HEADERS)
+#soup = BeautifulSoup(page.text, 'html.parser')
+#print(soup)
 #print(page.text)
+
+def download_matches(url, headers):
+    page = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    print(soup)
+
+
+
+download_matches(URL, HEADERS)
