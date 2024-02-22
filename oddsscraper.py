c4e622d1ba3037ef69eb74fd27c822c3a506bd84
tbog1997kom@gmail.com <tbog1997kom@gmail.com>
2024-02-22 03:22:16 -0500

    modified:   oddsscraper.py

--- oddsscraper.py
+++ oddsscraper.py
@@ -2,19 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
+
 HEADERS = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'}
 URL = "https://en.stoiximan.gr/sport/soccer/next-3-hours/"
 
 def download_matches(url, headers):
     driver = webdriver.Chrome()
     driver.get(URL)
-    matches_div = driver.find_elements(By.CLASS_NAME, "vue-recycle-scroller__item-wrapper")
-    matches_str = []
-    for match_div in matches_div:
-           matches_str.append(match_div.text)
-    driver.close()
-    return matches_str
+    driver.maximize_window()
     
-
 matches = download_matches(URL, HEADERS)
-print(matches)
+for match in matches:
+      print(match + "\n")
