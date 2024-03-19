279ea6d4cac467d633a16f350a71b56e7d434832
tbog1997kom@gmail.com <tbog1997kom@gmail.com>
2024-03-19 05:35:43 -0400

    modified:   oddsscraper.py

--- oddsscraper.py
+++ oddsscraper.py
@@ -17,7 +17,7 @@ class Match_DAT:
      match_time = ""
      match_over = ""
      match_under = ""
-     THRESHOLD = 20
+     THRESHOLD = 10
      def __init__(self, name, time, over, under):
           self.match_name = name
           self.match_time = time
