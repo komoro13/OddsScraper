46c89a84f483ad400df66a32b8833f897e8bf7c7
tbog1997kom@gmail.com <tbog1997kom@gmail.com>
2024-03-19 18:52:26 -0400

    modified:   oddsscraper.py

--- oddsscraper.py
+++ oddsscraper.py
@@ -69,7 +69,12 @@ def download_matches(url, headers):
     driver = webdriver.Chrome()
     driver.get(URL)
     driver.maximize_window()
-    driver.find_element(By.ID, COOKIES_ACCEPT_BTN).click()
+    driver.implicitly_wait(1)
+    cookies_btn = driver.find_element(By.ID, COOKIES_ACCEPT_BTN)
+    action = webdriver.ActionChains(driver)
+    action.move_to_element(cookies_btn)
+    action.click()
+    action.perform()
     scroll_y = 0
     matches_divs_array = []
     x = 0
